a8a9b91fdc358c5076032b59d8ab1f6b33c5ffe2
thomas <0milieux_member@icloud.com>
2024-06-21 09:27:13 +0200

    First commit after initializing the project.

---
+++ tests/__init__.py
@@ -0,0 +1,1 @@
+"""Unit test package for si_base_units_relationships_poster_recreate."""
---
+++ tests/test_true.py
@@ -0,0 +1,6 @@
+"""Tests for `si_base_units_relationships_poster_recreate` package."""
+
+
+def test_true() -> None:
+    """At least one test will always pass :)."""
+    assert True
